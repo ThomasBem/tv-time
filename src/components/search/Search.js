7b9806fb0fb6a6191c2f64dfe59a3220e9a188f0
Thomas <thomas.bembridge@fmcti.com>
2017-10-23 08:46:26 +0200

    Updated readme

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,11 +2,53 @@
 
 import React from 'react';
 
-class Search extends React.Component<{},{}> {
+const createSearchUrl = (query) => (`http://api.tvmaze.com/search/shows?q=${query}`);
+
+type State = {
+  tvShows: [],
+  query: string,
+}
+
+class Search extends React.Component<{}, State> {
+  constructor() {
+    super();
+    this.state = {
+      tvShows: [],
+      query: '',
+    };
+    this.updateQuery = this.updateQuery.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  updateQuery = (event: any) => {
+    this.setState({ query: event.target.value });
+  }
+
+  search = () => {
+    fetch(createSearchUrl(this.state.query))
+      .then(response => response.json())
+      .then(result => {
+        this.setState({ tvShows: result })
+      })
+      .catch(err => {
+        console.log('Error happened during fetching!', err);
+      });
+  }
+
   render() {
     return (
       <div>
-        Search
+        <input type="text" value={this.state.query} onChange={this.updateQuery} />
+        <button onClick={this.search}>Search</button>
+        {
+          this.state.tvShows.map(show => {
+            return (
+              <li key={show.show.id}>
+                <p>{show.show.name}</p>
+              </li>
+            )
+          })
+        }
       </div>
     );
   }
